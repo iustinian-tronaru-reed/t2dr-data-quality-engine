c7d159acf8bcf327aa0d24f8513008b5db08a865
Iustinian Tronaru <iustinian.tronaru@reed.com>
2026-08-24 14:57:16 +0100

    Corrected pandas function

--- src/extract.py
+++ src/extract.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 referrals = pd.read_csv("data/referrals.csv")
 contacts = pd.read_csv("data/contacts.csv")
-health_incidents = pd.read_CSV("data/health_incidents.csv")
+health_incidents = pd.read_csv("data/health_incidents.csv")
 
 print("Referrals")
 print(referrals.shape)
