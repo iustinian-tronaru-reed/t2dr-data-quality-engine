07192bd23743a50ee46d911e38424e1c2d5a6a6d
Iustinian Tronaru <iustinian.tronaru@reed.com>
2026-08-25 14:46:56 +0100

    Completed dimension schema DDL

--- sql/warehouse/create_fact_error.sql
+++ sql/warehouse/create_fact_error.sql
@@ -1,7 +1,25 @@
-CREATE TABLE FactError
+CREATE TABLE IF NOT EXISTS FactError
 (
-	ErrorKey INTEGER PRIMARY KEY,
-	RuleKey INTEGER,
-	ReferralKey INTEGER,
-	ErrorCount INTEGER
+    ErrorKey INTEGER PRIMARY KEY AUTOINCREMENT,
+    RuleKey INTEGER,
+    DatasetKey INTEGER,
+    DateKey INTEGER,
+    ReferralKey INTEGER,
+    ProgrammeKey INTEGER,
+    ErrorCount INTEGER DEFAULT 1,
+
+    FOREIGN KEY (RuleKey)
+        REFERENCES DimRule(RuleKey),
+
+    FOREIGN KEY (DatasetKey)
+        REFERENCES DimDataset(DatasetKey),
+
+    FOREIGN KEY (DateKey)
+        REFERENCES DimDate(DateKey),
+
+    FOREIGN KEY (ReferralKey)
+        REFERENCES DimReferral(ReferralKey),
+
+    FOREIGN KEY (ProgrammeKey)
+        REFERENCES DimProgramme(ProgrammeKey)
 );
